a323816853e969a0ac1d2d1223140113857d21cb
Antoine <lachaizeant@gmail.com>
2026-07-12 20:50:38 +0200

    Phase 2 complete: spells, mana management and combat action menu

--- game.py
+++ game.py
@@ -4,7 +4,13 @@ from enemies import Enemy
 #MAIN FIGHT FUNCTION LOOP
 def fight(hero, enemy):
     while hero.is_alive() and enemy.is_alive():
-        hero.attack(enemy)
+        print("1) Attack with your weapon")
+        print("2) Cast a spell")
+        choice = int (input("What do you want to do ?"))
+        if choice == 1:
+            hero.attack(enemy)
+        elif choice == 2:
+            hero.spell[0].cast(hero, enemy)
         if not enemy.is_alive():
             print (f"{hero.name} win the fight ! {enemy.name} was defeated !")
             break
